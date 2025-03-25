23855d058810a57da28961c7517642c8ff67e611
Annah Gaula Manda <165100703+Gaula26@users.noreply.github.com>
2025-03-25 07:55:11 +0200

    Added the factory and helper for payment and notification

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -29,6 +29,9 @@ public class User {
         this.userRole = builder.userRole;
     }
 
+    public User(Admin.Builder builder) {
+    }
+
     public String getUserIdentityNumber() {
         return userIdentityNumber;
     }
