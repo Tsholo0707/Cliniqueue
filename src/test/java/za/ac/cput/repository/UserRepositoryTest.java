45ccc2a6a768b710b0784378a83c8ef883495065
Agnes-99 <Agnesmabusela8@gmail.com>
2025-03-28 11:36:11 +0200

    Updated Repository test class for User

--- src/test/java/za/ac/cput/repository/UserRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/UserRepositoryTest.java
@@ -6,22 +6,25 @@ Date: 26/03/2025
 package za.ac.cput.repository;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import za.ac.cput.domain.User;
 import za.ac.cput.factory.UserFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestMethodsOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.MethodName.class)
 
 class UserRepositoryTest {
 
         private static IUserRepository repository = UserRepository.getRepository();
-        private User user = UserFactory.createUser("");
+        private User user = UserFactory.createUser("9892893872902", "Madikila","Agnes","Mabusela", "0762736725", "Agnes@gmail.com","Agnes12345","UNDEFINED");
 
     @Test
     void a_create() {
         User created = repository.create(user);
         assertNotNull(created);
+        assertEquals(user.getUserIdentityNumber(), created.getUserIdentityNumber());
         System.out.println(created);
     }
 
@@ -29,13 +32,15 @@ class UserRepositoryTest {
     void b_read() {
         User read = repository.read(user.getUserIdentityNumber());
         assertNotNull(read);
+        assertEquals(user.getUserIdentityNumber(), read.getUserIdentityNumber());
+        assertEquals(user.getUserFirstName(), read.getUserFirstName());
         System.out.println(read.toString());
     }
 
     @Test
     void c_update() {
-        User  newUser = new User.Builder().copy(user).setUserFirstName("").build();
-        User updated = repository.create(newUser);
+        User  updatedUser = new User.Builder().copy(user).setUserFirstName("Jenny").build();
+        User updated = repository.update(updatedUser);
         assertNotNull(updated);
         System.out.println(updated.toString());
     }
@@ -43,11 +48,13 @@ class UserRepositoryTest {
     @Test
     void d_delete() {
         assertTrue(repository.delete(user.getUserIdentityNumber()));
-        System.out.println("Success: employee deleted");
+        assertNotNull(repository.read(user.getUserIdentityNumber()));
+        System.out.println("Success: user deleted");
     }
 
     @Test
     void e_getAll() {
         System.out.println(repository.getAll());
+        assertTrue(repository.getAll().size() > 0);
     }
 }
