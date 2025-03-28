be49e10e44c5cea552180d666371c92daae40c9d
Agnes-99 <Agnesmabusela8@gmail.com>
2025-03-28 11:36:48 +0200

    Created Repository Interface for Admin

--- src/main/java/za/ac/cput/repository/IAdminRepository.java
+++ src/main/java/za/ac/cput/repository/IAdminRepository.java
@@ -4,12 +4,12 @@ Date: 26/03/2025
  */
 package za.ac.cput.repository;
 
-import za.ac.cput.domain.User;
+import za.ac.cput.domain.Admin;
 
 import java.util.List;
 
-public interface IAdminRepository extends IRepository< Admin, String > {
+public interface IAdminRepository extends IRepository<Admin, String > {
 
-    List<User> getAll();
+    List<Admin> getAll();
 
 }
